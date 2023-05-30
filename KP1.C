e15a293035a4b33771596f8bf9e3b74c3ed2139f
khushi <sangitapaladiya9@gmail.com>
2023-05-30 10:53:48 +0530

    first commit

---
+++ KP1.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+int A=1;
+clrscr();
+do{
+ printf("%d\n",A);
+ A++;
+}while(A<=10);
+getch();
+}
---
+++ KP2.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+int A;
+clrscr();
+
+for(A=10;A>=1;A--){
+   printf("%d\n",A);
+}
+getch();
+}
